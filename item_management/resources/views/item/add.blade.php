984b748b142e34220ce2c0df7d44f3ba0118282d
ShKo0818 <179200800+ShKo0818@users.noreply.github.com>
2025-02-04 20:16:08 +0900

    カテゴリ

--- item_management/resources/views/item/add.blade.php
+++ item_management/resources/views/item/add.blade.php
@@ -30,7 +30,7 @@
 
                         <<div class="form-group">
     <label for="category">カテゴリ</label>
-    <<select name="category" id="category" class="form-control" required>
+    <select name="category" id="category" class="form-control" required>
     <option value="">選択してください</option>
     @foreach($categories as $category)
         <option value="{{ $category->id }}" {{ old('category_id') == $category->id ? 'selected' : '' }}>
