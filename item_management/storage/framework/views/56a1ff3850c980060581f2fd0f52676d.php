a8bbc0f73fb444e1390eacc66fdc4fdfb2d33790
ShKo0818 <179200800+ShKo0818@users.noreply.github.com>
2025-02-04 15:42:14 +0900

    （訂正）実装

--- item_management/storage/framework/views/56a1ff3850c980060581f2fd0f52676d.php
+++ item_management/storage/framework/views/56a1ff3850c980060581f2fd0f52676d.php
@@ -7,7 +7,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="<?php echo e(csrf_token()); ?>">
 
-    <title><?php echo e(config('app.name', 'ログイン')); ?></title>
+    <title><?php echo e(config('app.name', 'Laravel')); ?></title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.bunny.net">
